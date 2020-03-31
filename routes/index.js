de18237bc47d5a73fdaf589723db06db21fd03f1
Henrique-Sousa <sh4dowonth3w4ll@gmail.com>
2020-03-31 17:21:07 -0300

    add sign up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var main_controller = require('../controllers/mainController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Clubhouse' });
 });
+
+router.get('/sign-up', main_controller.sign_up_get);
 
 module.exports = router;
