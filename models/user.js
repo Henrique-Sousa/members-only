f1c9c25f97967653f283bb847654cc678968d88e
Henrique-Sousa <sh4dowonth3w4ll@gmail.com>
2020-03-31 19:59:45 -0300

    separate fullname into first and last name

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	fullname: {type:String, required:true, max:100},
+	first_name: {type:String, required:true, max:100},
+	last_name: {type:String, required:true, max:100},
 	username: {type:String, required:true, max:100},
 	password: {type:String, required:true},
 	membership_status: {type:String, required:true}
