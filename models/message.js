e490988934082420018876f51de8a190c2b8183b
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-04-10 19:14:05 -0300

    formatting

--- models/message.js
+++ models/message.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-	title: {type:String, required:true},
-	text: {type:String, required:true},
-	author: {type:Schema.Types.ObjectId, ref: 'User', required:true},
-	timestamp: {type:Date, required:true}
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  timestamp: { type: Date, required: true },
 });
 
-module.exports = mongoose.model('Message', MessageSchema);
+module.exports = mongoose.model("Message", MessageSchema);
