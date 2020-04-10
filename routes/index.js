204d034f1ab60f621a761f8b03e479c0debb5afe
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-04-10 19:03:38 -0300

    add sign-up on post

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var main_controller = require('../controllers/mainController');
+var main_controller = require("../controllers/mainController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Clubhouse' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Clubhouse" });
 });
 
-router.get('/sign-up', main_controller.sign_up_get);
+router.get("/sign-up", main_controller.sign_up_get);
+router.post("/sign-up", main_controller.sign_up_post);
 
 module.exports = router;
