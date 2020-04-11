b76152854a3449616b07a937e8de75247695f471
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-04-11 03:30:22 -0300

    validate and sanitize some fields

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,38 @@
-exports.sign_up_get = function(req, res) {
-	res.render('sign-up-form');
-}
+const { body, validationResult } = require("express-validator");
 
-exports.sign_up_post = function(req, res) {
-	res.send('sign up on post');
-}
+exports.sign_up_get = function (req, res) {
+  res.render("sign-up-form");
+};
+
+exports.sign_up_post = [
+  body("first_name")
+    .notEmpty()
+    .trim()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters.")
+    .escape(),
+  body("last_name")
+    .notEmpty()
+    .trim()
+    .withMessage("Last name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Last name has non-alphanumeric characters.")
+    .escape(),
+  body("username")
+    .notEmpty()
+    .trim()
+    .withMessage("Username must be specified.")
+    .isAlphanumeric()
+    .withMessage("Username has non-alphanumeric characters")
+    .escape(),
+  //res.send("sign up on post");
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.send("ok");
+    } else {
+      res.send("error");
+    }
+  }
+];
