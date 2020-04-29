f64f5ecc85d6394647d63d5f1b59f22399991efe
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-04-29 16:57:49 -0300

    add Date.now as default timestamp

--- models/message.js
+++ models/message.js
@@ -6,7 +6,7 @@ const MessageSchema = new Schema({
   title: { type: String, required: true },
   text: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  timestamp: { type: Date, required: true },
+  timestamp: { type: Date, default: Date.now, required: true },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
