c0048046633cf0c2c87980a0bb499229effc9034
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-07-09 15:17:54 -0300

    remove express-generator boilerplate routes/users.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ if (process.env.NODE_ENV != 'production') {
 const User = require("./models/user");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const user = process.env.username;
 const pass = process.env.password;
@@ -69,7 +68,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
